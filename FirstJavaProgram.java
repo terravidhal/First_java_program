bc22e9c9bbf6242eed56ae6a6017b78f8d80f46e
terravidhal <infiniterraetincelle@gmail.com>
2023-10-03 05:07:29 +0100

    FirstJavaProgram

---
+++ FirstJavaProgram.java
@@ -0,0 +1,9 @@
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+
+       System.out.println("My name is Vidhal\nI am 25 years old\nMy hometown is Douala, Cameroun");
+
+	}
+}
